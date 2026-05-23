f251203337efe0fbf36b68b7c7947444ace34a1b
root <root@dgc-gabon.ga>
2026-05-23 17:46:15 +0000

    feat(dashboard): add Laravel Breeze admin dashboard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             DriverSeeder::class,
             VehicleSeeder::class,
+            AdminSeeder::class,
         ]);
     }
 }
